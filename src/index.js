cd0f05da0f9d8b2e1f0ac787dc9857c26336d06c
astro-israfil <astro.israfil.fi@gmail.com>
2024-06-10 13:47:29 +0600

    learn about utils code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 dotenv.config({path: "../env"})
 
 
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERROR:", error);
+            throw error;
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log('Server listening on port', process.env.PORT || 8000);
+        })
+    }).catch((error) => {
+        console.log("MongoDB connection faild: ", error);
+    });
+
+
 /*
 (async () => {
     try {
